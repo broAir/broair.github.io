14a6937815c63d815926d1e44fe907b993fbc88d
sheblykina <andreybobrovdev@gmail.com>
2016-03-28 21:32:58 +0800

    input is preserved. some texts

--- js/console.js
+++ js/console.js
@@ -1,26 +1,48 @@
 $(document).ready(function () {
     var commands = {
-        "help": function (args) {
+        "help": function (args, $output) {
             alert(args);
         }
-    }
+    };
+
+    var inputHtml = '<span id="input-text">root@anykeydev:~/projects $</span>\
+                      <span class="terminal-input" contenteditable="true">help</span>';
+
 
     var $hiddenInput = $(".terminal-helper");
     var $terminal = $(".terminal");
-    var $editLine = $(".terminal-edit");
+    var $inputLine = $(".terminal-input");
     var $output = $(".terminal-output");
+
+    var createNewInput = function () {
+        $inputLine.toggleClass("terminal-input");
+
+        $output.append(inputHtml);
+
+        $inputLine = $output.find(".terminal-input");
+    };
 
     $terminal.mouseup(function () {
         $hiddenInput.focus();
     });
 
+    $inputLine.mousedown($hiddenInput.focus());
 
-    $hiddenInput.keydown(function (e) {
+    $inputLine.focus(function (e) {
+        $hiddenInput.focus();
+    });
+
+    $hiddenInput.on("input", function (e) {
+        $inputLine.text($hiddenInput.text());
+    });
+
+    $hiddenInput.keypress(function (e) {
         var keycode = e.keyCode || e.which;
 
-        $editLine.text($hiddenInput.text());
         if (keycode == 13) {
-            var commandText = $editLine.text();
+            $inputLine.text($hiddenInput.text());
+
+            var commandText = $inputLine.text();
 
             if (commandText.trim() == "")
                 return;
@@ -28,10 +50,15 @@ $(document).ready(function () {
             var spaceIndex = commandText.indexOf(' ');
             var commandName = spaceIndex == -1 ? commandText : commandText.substring(0, spaceIndex);
             var commandArgs = spaceIndex == -1 ? null : commandText.substr(spaceIndex + 1);
+
             if (commands.hasOwnProperty(commandName)) {
-                commands[commandName](commandArgs);
+                commands[commandName](commandArgs, $output);
+                $output.append("<br />");
+                createNewInput();
             } else {
-                $output.append("<span>Command <b>" + commandName + "</b> not found</span><br/> <span> Type <b>help</b> to list all available commands </span> <br/>");
+                $output.append("<br /> <span>Command <b>" + commandName + "</b> not found</span><br/>");
+                $output.append("<span> Type <b>help</b> to list all available commands </span> <br/>");
+                createNewInput();
             }
 
             $hiddenInput.empty();
@@ -40,15 +67,9 @@ $(document).ready(function () {
                 $(child).remove();
             });
 
-            $editLine.empty();
+            $inputLine.empty();
         }
     });
 
-
-    $editLine.focus(function (e) {
-        $hiddenInput.focus();
-    });
-
-    $editLine.mousedown($hiddenInput.focus());
 
 });
