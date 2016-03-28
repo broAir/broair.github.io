9770ada31088bd03c05c84a3573d0395153192e6
broAir <andrey.bobrov.dev@gmail.com>
2016-03-28 18:02:12 +0800

    console in progress

--- js/console.js
+++ js/console.js
@@ -1,28 +1,71 @@
-function $(elid) {
-    return document.getElementById(elid);
-}
+$(document).ready(function () {
+    var commands = {
+        "help": function (args) {
+            alert(args);
+        }
 
-
-function dowrite(from, e) {
-    e = e || window.event;
-    var w = $(".edit");
-    var tw = from.value;
-    alert(tw);
-}
-
-function moveIt(count, e) {
-    e = e || window.event; var keycode = e.keyCode || e.which;
-    if (keycode == 37 && parseInt(cursor.style.left) >= (0 - ((count - 1) * 10))) {
-        cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
-    } else if (keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0) {
-        cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
     }
-    if (e.ctrlKey && e.keyCode == 65) {
-        $("writer").style.backgroundColor = "#99ccff";
-    } else {
-        $("writer").style.backgroundColor = "";
+    var $hiddenInput = $(".terminal-helper");
+    var $terminal = $(".terminal");
+    var $editLine = $(".terminal-edit");
+    var $output = $(".terminal-output");
+
+    $terminal.mouseup(function () {
+        $hiddenInput.focus();
+    });
+
+    $hiddenInput.keypress(function (e) {
+        $editLine.text($hiddenInput.text());
+    });
+
+    $hiddenInput.keydown(function (e) {
+        var keycode = e.keyCode || e.which;
+
+        if (keycode == 13) {
+            var commandText = $editLine.text();
+            var spaceIndex = commandText.indexOf(' ');
+            var commandName = spaceIndex == -1 ? commandText : commandText.substring(0, spaceIndex);
+            var commandArgs = spaceIndex == -1 ? null : commandText.substr(spaceIndex + 1);
+            if (commands.hasOwnProperty(commandName)) {
+                commands[commandName](commandArgs);
+            } else {
+                $output.append("<span>There is no such command <b>" + commandName + "</b></span><br/> <span> Type <b>help</b> for more info </span> <br/>");
+            }
+
+            $hiddenInput.empty();
+
+            $hiddenInput.children().each(function (e) {
+                $(e).remove();
+            });
+
+            $editLine.empty();
+        }
+    });
+
+
+    // from s/o
+    function placeCaretAtEnd(el) {
+        el.focus();
+        if (typeof window.getSelection != "undefined"
+                && typeof document.createRange != "undefined") {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            range.collapse(false);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (typeof document.body.createTextRange != "undefined") {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.collapse(false);
+            textRange.select();
+        }
     }
-}
-function alert(txt) {
-    console.log(txt);
-}
+
+    $editLine.focus(function (e) {
+        //placeCaretAtEnd($editLine.get(0));
+        $hiddenInput.focus();
+    });
+    $editLine.mousedown($hiddenInput.focus());
+
+});
